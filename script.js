93b501eb8fab35eceb5effa19214ecf5f573fddb
rutvik2266 <145542813+rutvik2266@users.noreply.github.com>
2023-09-20 13:35:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!'); 
+});
